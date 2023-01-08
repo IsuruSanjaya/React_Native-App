21272376448c679a8f0bd64fd8f63f707605b384
IsuruSanjaya <isurusanjayaa@gmail.com>
2023-01-08 13:20:54 +0530

    update the payment

--- src/screens/PaymentScreen/PaymentScreen.js
+++ src/screens/PaymentScreen/PaymentScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, TextInput, ImageBackground } from 'react-native'
 import React, { useState } from 'react'
 import CustomInput from '../../components/CustomInput';
 import visa from '../../../assets/images/visa.png';
@@ -13,53 +13,83 @@ export default function PaymentScreen() {
     const [cvc, setCvc] = useState('');
     const [date, setDate] = useState('');
     const [year, setYear] = useState('');
-    const onPayment=()=>{
+
+    const onPayment = () => {
         console.warn("fuck payment");
     }
 
 
 
     return (
-        <View style={styles.root}>
-            <CustomInput
-                value={cardNo}
-                setValues={setCardNo}
-                placeholder='Enter the Card Number'
 
-            />
-            <CustomInput
-                value={cvc}
-                setValues={setCvc}
-                placeholder="Enter the cvc Number"
+        <ImageBackground
+            source={require('../../../assets/images/app.jpg')}
+            style={styles.container}>
+            <View style={styles.root}>
 
-            />
-            <CustomInput
-                value={date}
-                setValues={setDate}
-                placeholder="date"
-            />
-            <CustomInput
-                value={year}
-                setValues={setYear}
-                placeholder="Year"
-            />
+                <Text style={styles.header}>School Payment</Text>
 
-            
 
-            <View style={styles.payimg}>
-                <Image source={visa} resizeMode="center" style={styles.visa}/>
-                <Image source={credit} resizeMode="center" style={styles.credit}/>
+                <CustomInput
+                    value={cardNo}
+                    setValues={setCardNo}
+                    placeholder='Enter the Card Number' />
+                <Text style={styles.label}>CVV</Text>
+
+                <CustomInput
+                    value={cvc}
+                    setValues={setCvc}
+                    placeholder="Enter the cvc Number" />
+
+
+
+                <View style={{
+                    flexDirection: 'row',
+                    alignItems: 'flex-start',
+                    height: 100
+                }}>
+                    <View style={styles.inputWrap}>
+                        <Text style={styles.label} >Expiration date</Text>
+                        <CustomInput style={styles.inputDate}
+                            placeholder="Expire Date"
+                            onPress={''}
+                            value={date}
+                            setValues={setDate}
+                        />
+                    </View>
+
+                    <View style={styles.inputWrap}>
+                        <Text style={styles.label}>CVV</Text>
+                        <CustomInput style={styles.inputCvv}
+                            maxLength={17}
+                            placeholder="CVV"
+                            onPress={''}
+                        />
+                    </View>
+                </View>
+
+
+
+
+                <View style={styles.payimg}>
+                    <Image source={visa} resizeMode="center" style={styles.visa} />
+                    <Image source={credit} resizeMode="center" style={styles.credit} />
+
+                </View>
+
+                <CustomButton
+                    text="Pay"
+                    onPress={onPayment}
+                    type="TERTIARY"
+                    fontWeight={10}
+                    />
+
+
 
             </View>
 
-            <CustomButton 
-          text="Pay" 
-          onPress={onPayment}
-          type="TERTIARY"
-          />
-       
-        </View>
 
+        </ImageBackground>
 
 
 
@@ -79,26 +109,66 @@ const styles = StyleSheet.create({
         marginTop: 10,
         margin: 50,
         justifyContent: "center",
-        flexWrap:"wrap",
-        padding:5,
-     
+        flexWrap: "wrap",
+        padding: 5,
 
-
-        
     },
     overContainer: {
         flex: 1,
     },
 
-    visa:{
-        height:50,
-        width:50,
-        flex:1,
+    visa: {
+        height: 50,
+        width: 50,
+        flex: 1,
     },
-    credit:{
-        height:50,
-        width:50,
-        flex:1,
+    credit: {
+        height: 50,
+        width: 50,
+        flex: 1,
+    },
+
+
+    row: {
+        flex: 1,
+        flexDirection: "row"
+
+    },
+    label: {
+        flex: 1,
+        fontWeight: 'bold'
+
+    },
+    inputWrap: {
+        flex: 1,
+        justifyContent: 'space-between',
+        flexDirection: 'column'
+    },
+    inputDate: {
+        flex: 1,
+        borderColor: "black"
+
+    },
+    inputCvv: {
+        flex: 1,
+
+    },
+
+    header: {
+        fontSize: 35,
+        color: 'black',
+        fontWeight: "bold",
+        fontStyle: "Arial",
+        borderColor: "black",
+        borderRadius: 5,
+        padding: 35
+
+    },
+
+    container: {
+        flex: 1,
+        width: '100%',
+        height: '100%'
     },
 
 })
